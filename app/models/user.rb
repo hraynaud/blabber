084090b8067c8a3869ce9fa62701165bdb40f6b4
hraynaud <herby.raynaud@gmail.com>
2016-04-04 11:43:22 -0700

    Adds conditional validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ActiveRecord::Base
   has_many :blabs
   validates_presence_of :uid, :handle
   has_secure_password
-  validates :password, :length => { :minimum => 5 }, allow_nil: true, if: ->(user){ user.handle.present?} 
+  validates :password, :length => { :minimum => 5 }, allow_nil: true, unless: :is_oauth?, on: :create
+
+  private
+
+  def is_oauth?
+    user.handle.present?
+  end
+
 end
