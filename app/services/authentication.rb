7715bde670182ce2a6586444cb24f79d73a0cfee
hraynaud <herby.raynaud@gmail.com>
2016-04-09 05:41:33 -0700

    Sets a random password for user created via oath

--- app/services/authentication.rb
+++ app/services/authentication.rb
@@ -12,7 +12,12 @@ class Authentication
   def self.login_by_oauth_token oauth, request_params
     request_token = OAuth::RequestToken.new(TWITTER, oauth.token, oauth.secret)
     access_token = request_token.get_access_token(oauth_verifier: request_params[:oauth_verifier])
-    user = User.find_or_create_by(uid: access_token.params[:user_id]) { |u| u.handle = access_token.params[:screen_name] }
+    user = User.find_or_create_by(uid: access_token.params[:user_id]) do |u| 
+      u.handle = access_token.params[:screen_name] 
+      #sets random password to avoid validation errors since email and password
+      #auth is also supported
+      u.password = u.password_confirmation = SecureRandom.urlsafe_base64(n=6) 
+    end
     jwt_for user
   end
 
@@ -20,5 +25,6 @@ class Authentication
     id = user.uid || user.id
     JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
   end
+  
 
 end
