e4c2d6da8d7fa631eacc417bb1ac9828b304e829
hraynaud <herby.raynaud@gmail.com>
2016-04-09 05:11:46 -0700

    Adds email to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404063139) do
+ActiveRecord::Schema.define(version: 20160409111815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160404063139) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "email"
   end
 
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
