e15d488d157701752d3e78f2bccaa0e1996f15be
hraynaud <herby.raynaud@gmail.com>
2017-02-19 13:44:45 -0800

    Use proc to set origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'localhost:8080' #Add production domain here
-
+     #origins 'localhost:8080' #Add production domain here
+     origins { |source, env| Rails.env.development? && source == "http://localhost:8080"}
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
