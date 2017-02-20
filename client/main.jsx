219a536c5f9d07dff8059c231beb06c8088ba598
hraynaud <herby.raynaud@gmail.com>
2017-02-20 09:15:43 -0800

    with babel 6 you need to add default when using commonJs require to get the default export

--- client/main.jsx
+++ client/main.jsx
@@ -7,7 +7,7 @@ var ReactDom = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var history = ReactRouter.browserHistory;
-var routes = require('./config/routes.jsx');
+var routes = require('./config/routes.jsx').default;
 
 ReactDom.render(<Router history={history}>{routes}</Router>,  document.getElementById("root"));
 
