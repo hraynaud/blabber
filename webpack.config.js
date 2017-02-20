96b0a3c8e7bf3510bef79e3c18ec9b76b38cf608
hraynaud <herby.raynaud@gmail.com>
2017-02-19 22:24:19 -0800

    Adds webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,19 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.jsx$/, loader: 'jsx-loader' }
+      { test: /\.jsx$/, loader: 'jsx-loader' },
+      {
+        test: /\.js$/, 
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   },
 
