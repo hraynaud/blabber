638c0136fc0b4e603370f2e533c22492845d8206
hraynaud <herby.raynaud@gmail.com>
2017-02-20 23:14:49 -0800

    moves routes js to app route

--- app_client/index.js
+++ app_client/index.js
@@ -5,7 +5,7 @@ require('./assets/blabs.css');
 import React from 'react';
 import {render} from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import routes from './config/routes.js';
+import routes from './routes.js';
 
 render(<Router history={browserHistory}>{routes}</Router>,  document.getElementById("root"));
 
