c612665571b3261f9af8e86cb0645a7320ecda36
hraynaud <herby.raynaud@gmail.com>
2017-02-20 23:21:41 -0800

    renames AboutView to About

--- app_client/routes.js
+++ app_client/routes.js
@@ -4,13 +4,13 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App.jsx';
 import LoginForm from './components/Login.jsx';
 import BlabsViewContainer from './components/blabs/ViewContainer.jsx';
-import AboutView from './components/AboutView.jsx';
+import About from './components/About.jsx';
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={BlabsViewContainer} />
-    <Route path="about" component={AboutView} />
+    <Route path="about" component={About} />
     <Route path="login" component={LoginForm} />
   </Route>
 );
