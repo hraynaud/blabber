00df17c8261299a1d43aa640302e7f9a35bb7646
hraynaud <herby.raynaud@gmail.com>
2017-02-20 22:16:20 -0800

    convert routes to plain js file extension

--- app_client/index.js
+++ app_client/index.js
@@ -5,7 +5,7 @@ require('./assets/blabs.css');
 import React from 'react';
 import {render} from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import routes from './config/routes.jsx';
+import routes from './config/routes.js';
 
 render(<Router history={browserHistory}>{routes}</Router>,  document.getElementById("root"));
 
